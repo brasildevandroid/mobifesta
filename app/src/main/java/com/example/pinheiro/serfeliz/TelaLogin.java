4108bee0853c2b9a84966d6eaf90a648d5322f70
luiz <luizcox1@hotmail.com>
2018-06-23 19:37:25 -0300

    configuramos o login do facebook

---
+++ app/src/main/java/com/example/pinheiro/serfeliz/TelaLogin.java
@@ -0,0 +1,16 @@
+package com.example.pinheiro.serfeliz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_login);
+
+
+
+    }
+}
